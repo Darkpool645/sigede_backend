9d24106426cd79a6a7250e82af3db6730649774e
Darkpool645 <angel_camargo2020@outlook.com>
2024-10-17 12:31:05 -0600

    [CHORE] Add initial project setup and example properties

---
+++ src/main/java/mx/edu/utez/sigede_backend/SigedeBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sigede_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigedeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigedeBackendApplication.class, args);
+	}
+
+}
