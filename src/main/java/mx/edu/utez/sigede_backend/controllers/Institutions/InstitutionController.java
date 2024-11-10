706e5dfb4615aa3e014ef990b6cde5d8dc0293e3
EmilioVejarDiaz <20213tn091@utez.edu.mx>
2024-11-09 19:46:14 -0600

    [ADD] Gestión de clientes - Update clientes: Modificación de la información del cliente
    Modificación de la información del cliente y manejo del status de este

--- src/main/java/mx/edu/utez/sigede_backend/controllers/Institutions/InstitutionController.java
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/Institutions/InstitutionController.java
@@ -1,7 +1,11 @@
 package mx.edu.utez.sigede_backend.controllers.Institutions;
 
+import mx.edu.utez.sigede_backend.controllers.Institutions.DTO.InstitutionStatusUpdateDTO;
+import mx.edu.utez.sigede_backend.controllers.Institutions.DTO.InstitutionUpdateDTO;
 import mx.edu.utez.sigede_backend.controllers.Institutions.DTO.PostInstitutionDTO;
 import mx.edu.utez.sigede_backend.utils.CustomResponse;
+import mx.edu.utez.sigede_backend.utils.exception.CustomException;
+import mx.edu.utez.sigede_backend.utils.exception.ErrorDictionary;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,16 +15,17 @@ import mx.edu.utez.sigede_backend.services.institution.InstitutionService;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/institutions")
 public class InstitutionController {
 
     private final InstitutionService institutionService;
+    private final ErrorDictionary errorDictionary;
 
-    public InstitutionController(InstitutionService institutionService) {
+    public InstitutionController(InstitutionService institutionService, ErrorDictionary errorDictionary) {
         this.institutionService = institutionService;
+        this.errorDictionary = errorDictionary;
     }
 
     @GetMapping
@@ -41,5 +46,48 @@ public class InstitutionController {
         Institution institution =institutionService.postInstitution(payload);
         return new CustomResponse<>(HttpStatus.CREATED.value(), "Institution creada correctamente.",
                 false, institution);
+    }
+
+    @PutMapping("/update-institution")
+    public ResponseEntity<CustomResponse<Institution>> updateInstitution(
+            @Validated @RequestBody InstitutionUpdateDTO payload) {
+        try {
+            Institution updatedInstitution = institutionService.updateInstitution(payload);
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.OK.value(), "Institution actualizada correctamente.", false, updatedInstitution);
+            return ResponseEntity.ok(response);
+        } catch (CustomException e) {
+            String errorMessage = errorDictionary.getErrorMessage(e.getErrorCode());
+
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.BAD_REQUEST.value(), errorMessage, true, null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        } catch (Exception e) {
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), "Ocurrió un error inesperado.", true, null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @PutMapping("/update-institution-status")
+    public ResponseEntity<CustomResponse<Institution>> updateInstitutionStatus(
+            @RequestBody InstitutionStatusUpdateDTO payload) {
+        try {
+            Institution updatedInstitution = institutionService.updateInstitutionStatus(
+                    payload.getInstitutionId(), payload.getInstitutionStatus());
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.OK.value(), "Status de la institución actualizado correctamente.", false, updatedInstitution);
+            return ResponseEntity.ok(response);
+        } catch (CustomException e) {
+            String errorMessage = errorDictionary.getErrorMessage(e.getErrorCode());
+
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.BAD_REQUEST.value(), errorMessage, true, null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        } catch (Exception e) {
+            CustomResponse<Institution> response = new CustomResponse<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), "Ocurrió un error inesperado.", true, null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
--- src/main/java/mx/edu/utez/sigede_backend/models/institution/InstitutionRepository.java
+++ src/main/java/mx/edu/utez/sigede_backend/models/institution/InstitutionRepository.java
@@ -1,5 +1,4 @@
 package mx.edu.utez.sigede_backend.models.institution;
-import java.util.UUID;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
