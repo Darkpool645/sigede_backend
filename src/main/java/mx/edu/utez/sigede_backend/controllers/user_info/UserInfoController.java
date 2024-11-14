12a24c79b382db0ba6ee5e89364e7c4c95fe7055
Kellguzdi <20213tn114@utez.edu.mx>
2024-11-14 00:17:55 -0600

    [Update] ManagementUserInfo - controller

--- src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/UserInfoController.java
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/UserInfoController.java
@@ -1,10 +1,26 @@
 package mx.edu.utez.sigede_backend.controllers.user_info;
 
+import mx.edu.utez.sigede_backend.controllers.user_info.dto.RequestNewUserInfoDTO;
+import mx.edu.utez.sigede_backend.services.user_info.UserInfoService;
+import mx.edu.utez.sigede_backend.utils.CustomResponse;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user-info")
 public class UserInfoController {
-    
+    private final UserInfoService service;
+
+    public UserInfoController(UserInfoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/save-data")
+    public CustomResponse<String> postUserInfo(@Validated @RequestBody RequestNewUserInfoDTO payload){
+        service.saveUserInfo(payload);
+        return new CustomResponse<>(201,"Información registrada correctamente",false,null);
+    }
 }
