41ec6405ec1493255911d1df0ad5084cfa1893a1
Kellguzdi <20213tn114@utez.edu.mx>
2024-11-14 00:17:29 -0600

    [ADD[ ManagementUserInfo - saveData: use a saveUserInfo service

--- src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/dto/RequestNewUserInfoDTO.java
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/dto/RequestNewUserInfoDTO.java
@@ -1,15 +1,23 @@
 package mx.edu.utez.sigede_backend.controllers.user_info.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class RequestNewUserInfoDTO {
+    @NotNull(message = "isincard.notnull")
     private boolean is_in_card;
+    @NotNull(message = "isinqr.notnull")
     private boolean is_in_qr;
+    @NotBlank(message = "tag.notnull")
+    @Size(max = 50, message = "tag.size")
     private String tag;
+    @NotNull(message = "type.notnull")
     private String type;
 }
