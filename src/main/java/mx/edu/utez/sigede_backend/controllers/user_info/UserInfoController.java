e2858df8d5ca698cba0075be91c9fdab5356f9ba
Kellguzdi <20213tn114@utez.edu.mx>
2024-11-13 23:47:58 -0600

    [ADD] ManagementUserInfo - controller - create controller

---
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/user_info/UserInfoController.java
@@ -0,0 +1,10 @@
+package mx.edu.utez.sigede_backend.controllers.user_info;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user-info")
+public class UserInfoController {
+    
+}
