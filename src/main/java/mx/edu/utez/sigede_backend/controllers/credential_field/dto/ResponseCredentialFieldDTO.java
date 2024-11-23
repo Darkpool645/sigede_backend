1b6c00ac595c42672a28db97d2cf9a1d97067408
Kellguzdi <20213tn114@utez.edu.mx>
2024-11-22 21:34:36 -0600

    [Update] ManagementUserInfo - DTO: get All User Info with a credential relationship

--- src/main/java/mx/edu/utez/sigede_backend/controllers/credential_field/dto/ResponseCredentialFieldDTO.java
+++ src/main/java/mx/edu/utez/sigede_backend/controllers/credential_field/dto/ResponseCredentialFieldDTO.java
@@ -10,10 +10,15 @@ import mx.edu.utez.sigede_backend.models.credential_field.CredentialField;
 public class ResponseCredentialFieldDTO {
     private String value;
     private String tag;
-
+    private String type;
+    private boolean is_in_card;
+    private boolean is_in_qr;
 
     public ResponseCredentialFieldDTO(CredentialField field) {
         this.value = field.getValue();
         this.tag = field.getFkUserInfo().getTag();
+        this.type = field.getFkUserInfo().getType();
+        this.is_in_card = field.getFkUserInfo().isInCard();
+        this.is_in_qr = field.getFkUserInfo().isInQr();
     }
 }
